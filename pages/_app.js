d560b536a45c013d1389dd7eda5bc786abe3bc94
Gab <gabtan99@gmail.com>
2021-10-31 17:40:23 +0800

    test: no font

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,6 @@ import Fonts from "../styles/fonts";
 function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
-      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
