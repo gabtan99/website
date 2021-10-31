07c02597ebea83a580557a4e417b04c89d481282
Gab <gabtan99@gmail.com>
2021-10-31 17:53:29 +0800

    revert: styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import "../styles/globals.css";
-import { ChakraProvider, CharkraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layouts/main";
 import theme from "../styles/theme";
 import Fonts from "../styles/fonts";
 
 function MyApp({ Component, pageProps, router }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
